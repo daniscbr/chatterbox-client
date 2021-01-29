44effa0bda11d7ae897e0faaf017fa40c5173be3
Daniela <dani.scbr0@gmail.com>
2021-01-29 13:30:18 -0600

    Passing creating ajax behavior

--- client/scripts/messagesView.js
+++ client/scripts/messagesView.js
@@ -3,9 +3,20 @@ var MessagesView = {
   $chats: $('#chats'),
 
   initialize: function() {
+    //var messagesArray  = readAll(successCB, failureCB)
+    //return messagesArray
   },
 
   render: function() {
+    //var messageData = initialize();
+    //loop over message data instead of movies below
+    var i; var html = '';
+    for (i = 0; i < data.movies.length; i++) {
+      html += compiled(data.movies[i]);
+    }
+    $('#movies').append(html);
   }
 
 };
+
+
